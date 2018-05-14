9144ffa92c6a509de68a667164b989e8c5baf72a
Rowland <rowland.willets@gmail.com>
2018-05-14 22:16:58 +0100

    gulp error handling & addition of code comments

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -8,5 +8,9 @@ cssImport = require('postcss-import');
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
     .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .on('error', function(errorInfo) {
+      console.log(errorInfo.toString());    /* errorInfo is an object, toString makes it more human readable */
+      this.emit('end');                     /* end - end task silently */
+    })
     .pipe(gulp.dest('./app/temp/styles'));
 });
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,13 +1,14 @@
 var gulp = require('gulp'),
 watch = require('gulp-watch'),
-browserSync = require('browser-sync').create();
+browserSync = require('browser-sync').create();       /* Import just the Create method */
 
 gulp.task('watch', function() {
 
+  /* browserSync actually spins up a little web server on our computer */
   browserSync.init({
-    notify: false,
+    notify: false,        /* some browsers have visual notification warning whenever a CSS injection occurs */
     server: {
-      baseDir: "app"
+      baseDir: "app"      /* where index.html lives */
     }
   });
 
@@ -21,7 +22,12 @@ gulp.task('watch', function() {
 
 });
 
+/* When it comes to CSS browserSync can actually inject our latest CSS into the page
+   without even forcing a refresh */
+/* 2nd arg - dependancies : run by gulp BEFORE running the anonymous function */
 gulp.task('cssInject', ['styles'], function() {
   return gulp.src('./app/temp/styles/styles.css')
     .pipe(browserSync.stream());
 });
+
+/* unlike reload, BrowserSync Inject preserves text selections in HTML!! */
